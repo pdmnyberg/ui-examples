30f427dfc9d6104881872571113c80e4337c42a8
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-10-16 17:12:59 +0200

    fix: update bird-ringing home

--- ui-examples/src/app/bird-ringing/page.tsx
+++ ui-examples/src/app/bird-ringing/page.tsx
@@ -1,4 +1,14 @@
+import Link from "next/link";
 
 export default function Home() {
-  return <span>Hello world</span>
+  return (
+    <div className="container">
+      <h2>Bird Ringing Example Pages</h2>
+      <p>The following are example pages to demonstrate layout an interaction behaviors. These document do not aim to show anything related to a final design.</p>
+      <ul>
+        <li><Link href="/bird-ringing/list-view">List view</Link></li>
+        <li><Link href="/bird-ringing/entry-view/?ringerId=ringer-0">Entry view</Link></li>
+      </ul>
+    </div>
+  )
 }
