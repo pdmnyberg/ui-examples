083591096c082202ed3cc34a4ffa7fc281269392
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-11-06 17:33:09 +0100

    feat: add metrics question selection

--- ui-examples/src/app/tmd/page.tsx
+++ ui-examples/src/app/tmd/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import useSWR from 'swr';
-import { useState, useRef, useCallback, Fragment } from "react";
+import { useState, useRef, useCallback, ChangeEventHandler, useEffect } from "react";
 
 type Entry = {
   id: string;
@@ -16,22 +16,21 @@ type QuestionData = {
 type MetricsData = {
   label: string;
   id: string;
-  options: Entry & {
+  options: (Entry & {
     count: number;
-  }[]
+  })[]
 }
 
 type Result<T> = {
   values: T[]
 }
 
-async function fetchMetrics([apiRoot, questionset, questions]: [string, string, string[]]): Promise<Result<MetricsData>> {
-  console.log(questions)
-  return await (await fetch(`${apiRoot}/metrics/set/${questionset}?${questions.map(q => `questions=${q}`).join("&")}`)).json()
+async function fetchMetrics([apiRoot, questions]: [string, string[]]): Promise<Result<MetricsData>> {
+  return await (await fetch(`${apiRoot}/metrics/questions?${Array.from(questions).map(q => `ids=${q}`).join("&")}`)).json()
 }
 
 async function fetchQuestions([apiRoot, questionset]: [string, string[]]): Promise<Result<QuestionData>> {
-  return await (await fetch(`${apiRoot}/properties/set/${questionset}`)).json()
+  return await (await fetch(`${apiRoot}/properties/sets?ids=${questionset}`)).json()
 }
 
 const viewAlternatives: Entry[] = [
@@ -45,15 +44,13 @@ const questionsets: Entry[] = [
   {label: "Demographic", id: "demographic"},
 ]
 
-const questionsetIds = questionsets.map(q => q.id)
-
 export default function Home() {
   const [apiRoot, setAPIRoot] = useState("http://localhost:8000")
   const [view, setView] = useState("questions")
   const [questionset, setQuestionset] = useState(questionsets[0].id);
-  const [questions, setQuestions] = useState(["quality-used_resources_before", "quality-used_resources_future"]);
+  const [questions, setQuestions] = useState<Set<string>>(new Set());
   const {data: questionData, error: questionError, isLoading: questionLoading} = useSWR([apiRoot, questionset], fetchQuestions);
-  const {data, error: dataError, isLoading: dataLoading} = useSWR([apiRoot, questionset, questions], fetchMetrics);
+  const {data, error: dataError, isLoading: dataLoading} = useSWR([apiRoot, questions], fetchMetrics);
   const apiRootInputRef = useRef<HTMLInputElement>(null);
   const updateRoot = useCallback(() => {
     const input = apiRootInputRef.current;
@@ -61,12 +58,29 @@ export default function Home() {
       setAPIRoot(input.value)
     }
   }, [apiRootInputRef, setAPIRoot])
+  const handleQuestionChoice = useCallback<ChangeEventHandler<HTMLInputElement>>((event) => {
+    const entryId = event.target.getAttribute("data-entry-id");
+    const checked = event.target.checked;
+    if (entryId) {
+      setQuestions((questions) => {
+        if (checked) {
+          questions.add(entryId)
+        } else {
+          questions.delete(entryId)
+        }
+        return new Set(questions)
+      })
+    }
+  }, [setQuestions])
+  useEffect(() => {
+    setQuestions(new Set())
+  }, [questionset])
   console.log(data, dataError, dataLoading)
   console.log(questionData, questionError, questionLoading)
   return (
     <div className="container">
       <h2>TMD API Example</h2>
-      <div className="row mb-3">
+      <div className="row my-3">
         <div className="col">
           <div className="input-group">
             <input type="text" ref={apiRootInputRef} name="api-root" defaultValue={apiRoot} />
@@ -74,7 +88,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="row">
+      <div className="row my-3">
         <div className="col">
           <div className="btn-group btn-group-toggle" data-toggle="buttons">
             {viewAlternatives.map(v => (
@@ -90,6 +104,16 @@ export default function Home() {
           </select>
         </div>
       </div>
+      {view === "metrics" && questionData && (
+        <div className="row my-3">
+          {questionData.values.map(question => (
+            <div className="col-2" key={question.id}>
+              <input className="form-check-input" type="checkbox" id={question.id} data-entry-id={question.id} onChange={handleQuestionChoice} checked={questions.has(question.id)}/>
+              <label htmlFor={question.id}>{question.label}</label>
+            </div>
+          ))}
+        </div>
+      )}
       <hr />
       {view === "questions" && (
         <>
@@ -121,7 +145,31 @@ export default function Home() {
       {view === "metrics" && (
         <>
           <h3>Metrics view</h3>
-          
+          {data && data.values.map(question => (
+            <div className="row" key={question.id}>
+              <div className="col">
+                <h4>{question.label}</h4>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">ID</th>
+                      <th scope="col">Label</th>
+                      <th scope="col">Count</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {question.options.map(option => (
+                      <tr key={option.id}>
+                        <th>{option.id}</th>
+                        <td>{option.label}</td>
+                        <td>{option.count}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ))}
         </>
       )}
     </div>
