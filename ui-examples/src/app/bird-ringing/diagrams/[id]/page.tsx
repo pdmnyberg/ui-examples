6cbdf57e9ecfe3d130b5e41706bf592959c46667
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-11-12 15:37:07 +0100

    fix: use fluid container for diagram content

--- ui-examples/src/app/bird-ringing/diagrams/[id]/page.tsx
+++ ui-examples/src/app/bird-ringing/diagrams/[id]/page.tsx
@@ -7,12 +7,16 @@ import path from "path";
 
 function DiagramBase({src, id}: {src: string, id?: string}) {
   return (
-    <div className="container">
-      <Warning>
-        <p>Using flowcharts from <a href="https://mermaid.js.org/syntax/flowchart.html">mermaid</a>. Also consider using sequence diagram from <a href="https://mermaid.js.org/syntax/sequenceDiagram.html">mermaid</a>.</p>
-      </Warning>
-      <MermaidDiagram src={src} id={id} />
-    </div>
+    <>
+      <div className="container">
+        <Warning>
+          <p>Using flowcharts from <a href="https://mermaid.js.org/syntax/flowchart.html">mermaid</a>. Also consider using sequence diagram from <a href="https://mermaid.js.org/syntax/sequenceDiagram.html">mermaid</a>.</p>
+        </Warning>
+      </div>
+      <div className="container-fluid">
+        <MermaidDiagram src={src} id={id} />
+      </div>
+    </>
   )
 }
 
