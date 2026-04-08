c50b57600ca934d7c88f0c0f2a5bda2480a39103
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-04-08 14:09:35 +0200

    refactor: share diagram page with bird ringing

--- ui-examples/src/app/bird-ringing/diagrams/[id]/page.tsx
+++ ui-examples/src/app/bird-ringing/diagrams/[id]/page.tsx
@@ -1,11 +1,7 @@
 import Warning from "../../warning";
-import { Suspense } from "react";
-import { MermaidDiagram } from "@/components/MermaidDiagram";
-import fs from "fs";
-import path from "path";
+import { DiagramPage, generateStaticParams as baseGenerateStaticParams } from "@/components/DiagramPage";
 
-
-function DiagramBase({src, id}: {src: string, id?: string}) {
+export default async function Page(args: {params: Promise<{ id: string }>}) {
   return (
     <>
       <div className="container">
@@ -13,29 +9,11 @@ function DiagramBase({src, id}: {src: string, id?: string}) {
           <p>Using flowcharts from <a href="https://mermaid.js.org/syntax/flowchart.html">mermaid</a>. Also consider using sequence diagram from <a href="https://mermaid.js.org/syntax/sequenceDiagram.html">mermaid</a>.</p>
         </Warning>
       </div>
-      <div className="container-fluid">
-        <MermaidDiagram src={src} id={id} />
-      </div>
+      {DiagramPage(args)}
     </>
-  )
+  );
 }
 
 export function generateStaticParams() {
-  const paths = fs.readdirSync("public/diagrams")
-    .map(file => path.basename(file))
-    .filter(name => name.endsWith(".mermaid"))
-    .map(name => ({id: name.replace(".mermaid", "")}));
-  return paths;
-};
-
-
-export default async function Page({params}: {params: Promise<{ id: string }>}) {
-  const {id} = await params;
-  const basePath = process.env.NEXT_PUBLIC_API_URL || "";
-  const src = `${basePath}/diagrams/${id}.mermaid`;
-  return (
-    <Suspense>
-      <DiagramBase src={src} id={id}/>
-    </Suspense>
-  )
+  return baseGenerateStaticParams();
 }
---
+++ ui-examples/src/components/DiagramPage.tsx
@@ -0,0 +1,83 @@
+import { Suspense } from "react";
+import { MermaidDiagram } from "@/components/MermaidDiagram";
+import fs from "fs";
+import path from "path";
+import fm from "front-matter";
+import Markdown from 'react-markdown'
+
+
+function DiagramBase({src, id}: {src: string, id?: string}) {
+  return (
+    <>
+      <div className="container-fluid">
+        <MermaidDiagram src={src} id={id} />
+      </div>
+    </>
+  )
+}
+
+const dataDir = "public/diagrams";
+
+export function generateStaticParams(filter: (name: string) => boolean = () => true) {
+  const paths = fs.readdirSync(dataDir)
+    .map(file => path.basename(file))
+    .filter(name => name.endsWith(".mermaid"))
+    .filter(name => filter(name))
+    .map(name => ({id: name.replace(".mermaid", "")}));
+  return paths;
+};
+
+function getAttr(obj: unknown, attr: string) {
+  return obj && typeof obj === "object" && attr in obj ? obj[attr as keyof typeof obj] as unknown : undefined;
+}
+
+function parseString(obj: unknown, attr: string): string {
+  const attribute = getAttr(obj, attr)
+  if (attribute) {
+    return String(attribute)
+  }
+  return "";
+}
+
+function parseDescriptions(obj: unknown, attr: string): {title: string, content: string}[] {
+  const attribute = getAttr(obj, attr)
+  if (Array.isArray(attribute)) {
+    return attribute.map(item => ({
+      title: parseString(item, "title"),
+      content: parseString(item, "content")
+    }))
+  }
+  return []
+}
+
+
+export async function DiagramPage({params}: {params: Promise<{ id: string }>}) {
+  const {id} = await params;
+  const basePath = process.env.NEXT_PUBLIC_API_URL || "";
+  const src = `${basePath}/diagrams/${id}.mermaid`;
+  const data = fs.readFileSync(`${dataDir}/${id}.mermaid`, "utf8");
+  const content = fm(data);
+  const attributes = typeof content.attributes === "object" && content.attributes !== null ?  content.attributes : {};
+  const descriptions = parseDescriptions(attributes, "descriptions");
+
+  return (
+    <>
+      {"title" in attributes ? <h2>{String(attributes.title)}</h2> : undefined}
+      <Suspense>
+        <DiagramBase src={src} id={id}/>
+      </Suspense>
+      <div className="row px-2">
+      {descriptions.map((item, index) => (
+        <div className="col-sm-4 p-2" key={index}>
+          <div className="card">
+            <div className="card-header">{item.title}</div>
+            <div className="card-body">
+              <Markdown>{item.content}</Markdown>
+            </div>
+          </div>
+        </div>
+      ))}
+      </div>
+    </>
+  )
+}
