44eea83809935b2ea877cbe5a664574a66c5e8b7
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-04-13 15:28:26 +0200

    fix: add initial filtering

--- ui-examples/src/app/sdad/datasets/entry/page.tsx
+++ ui-examples/src/app/sdad/datasets/entry/page.tsx
@@ -1,15 +1,22 @@
 "use client"
 
 import { useSearchParams, notFound } from "next/navigation";
-import { Suspense } from "react";
+import { CSSProperties, Suspense, useState } from "react";
 import Link from "next/link";
 import { useData } from "../../contexts";
 import { EntityRef, toLocalDate } from "@/app/common";
 import { Pagination, usePagination } from "@/components/Pagination";
 import { ColumnSpec, DataSpec, Table, VerticalTable } from "@/components/Table";
 import { Dataset, DatasetFile } from "../../types";
+import { TypedSearchableItem, useFilter, useItemSelections } from "@/app/hooks";
 
-type DatasetFileTable = DataSpec<DatasetFile>;
+const dropdownOpenStyle: CSSProperties = {
+  position: "absolute",
+  inset: "0px 0px auto auto",
+  margin: "0px",
+  transform: "translate(0px, 40px)",
+}
+
 type DatasetTable = DataSpec<Dataset & {datasetId: string}>;
 
 function EntryViewBase() {
@@ -19,14 +26,33 @@ function EntryViewBase() {
   if (!datasetId) {
     notFound();
   }
+  const [actionIsOpen, setActionIsOpen] = useState(false); 
   const datasetRef: EntityRef<"dataset"> = {id: datasetId, type: "dataset"};
   const dataset = datasets.get(datasetRef);
-  const datasetFiles = files.find({properties: {dataset: datasetRef}}).map<DatasetFileTable>((f) => ({
+  const datasetFiles = files.find({properties: {dataset: datasetRef}}).map<TypedSearchableItem<DatasetFile>>((f) => ({
     id: f.id,
-    filePath: f.filePath,
-    decryptedSize: `${f.decryptedSize} bytes`,
-    downloadUrl: <Link href={f.downloadUrl}>Download</Link>,
-    checksums: f.checksums.join(", ")
+    properties: {
+      id: {
+        term: f.id,
+        component: <Link href={`/datasets/entry/file/?entryId=${f.id}`}>{f.id}</Link>,
+      },
+      filePath: {
+        term: f.filePath,
+        component: f.filePath,
+      },
+      decryptedSize: {
+        term: `${f.decryptedSize} bytes`,
+        component: `${f.decryptedSize} bytes`,
+      },
+      downloadUrl: {
+        term: "",
+        component: <Link href={f.downloadUrl}>Download</Link>
+      },
+      checksums: {
+        term: f.checksums.join(", "),
+        component: f.checksums.join(", "),
+      }
+    }
   }));
   const datasetEntry: DatasetTable = {
     id: dataset.id,
@@ -36,8 +62,9 @@ function EntryViewBase() {
     size: String(dataset.size),
   }
 
-  const filesPagination = usePagination(datasetFiles, 30);
-  const datasetFileColumns: ColumnSpec<DatasetFile> = {
+  const datasetFileColumns: ColumnSpec<DatasetFile & {select: string}> = {
+    select: "",
+    id: "File",
     filePath: "File path",
     decryptedSize: "Decrypted size",
     downloadUrl: "Download",
@@ -49,6 +76,26 @@ function EntryViewBase() {
     files: "Number of files",
     size: "Dataset size",
   };
+
+  const {filter, setFilter, filteredItems} = useFilter(datasetFiles)
+  const {
+    selectedItems,
+    toggleItems,
+    allSelected,
+    handleItemSelection
+  } = useItemSelections(new Set(filteredItems.map(r => r.id)));
+
+  const filesPagination = usePagination(filteredItems, 30);
+
+  const selectablePageItems = filesPagination.items.map(item => ({
+    ...item,
+    properties: {
+      ...item.properties,
+      select: {
+        component: <input type="checkbox" onChange={handleItemSelection} checked={selectedItems.has(item.id)} data-actor-id={item.id}/>
+      },
+    }
+  }))
   
   return (
     <div className="container">
@@ -56,8 +103,29 @@ function EntryViewBase() {
       <h3>Properties</h3>
       <VerticalTable items={[datasetEntry]} columns={datasetColumns} param="Param"/>
       <h3>Files</h3>
+      <div className="input-group mb-3">
+        <span className="input-group-text">Filter</span>
+        <input
+          type="text"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+          className="form-control"
+          placeholder={Object.values(datasetFileColumns).filter(c => !!c).join(", ")}
+          aria-label="Filter for actor table"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div className="input-group mb-3">
+        <button className="btn btn-outline-secondary" type="button" onClick={toggleItems}>{allSelected ? "Select None" : "Select All"}</button>
+        <span className="input-group-text flex-grow-1" >{selectedItems.size} of {datasetFiles.length} selected</span>
+        <button className="btn btn-outline-secondary dropdown-toggle" onClick={() => setActionIsOpen(!actionIsOpen)} type="button" aria-expanded={actionIsOpen}>File actions</button>
+        <ul className={`dropdown-menu ${actionIsOpen ? "show" : ""}`} style={actionIsOpen ? dropdownOpenStyle : {}} onClick={() => setActionIsOpen(false)}>
+          <li><a className="dropdown-item" href="#">Download files</a></li>
+          <li><a className="dropdown-item" href="#">Download checksums</a></li>
+        </ul>
+      </div>
       <Pagination pages={filesPagination.pages} currentPage={filesPagination.currentPage}/>
-      <Table items={filesPagination.items} columns={datasetFileColumns} />
+      <Table items={selectablePageItems} columns={datasetFileColumns} />
       <Pagination pages={filesPagination.pages} currentPage={filesPagination.currentPage}/>
     </div>
   )
--- ui-examples/src/app/sdad/datasets/page.tsx
+++ ui-examples/src/app/sdad/datasets/page.tsx
@@ -1,23 +1,44 @@
 "use client"
 
 import { ColumnSpec, DataSpec, Table } from "@/components/Table";
-import React from "react";
+import React, { CSSProperties, useState } from "react";
 import { useData } from "../contexts";
 import { Pagination, usePagination } from "@/components/Pagination";
 import Link from "next/link";
 import { toLocalDate } from "@/app/common";
 import { Dataset } from "../types";
+import { TypedSearchableItem, useFilter } from "@/app/hooks";
 
-type DatasetTable = DataSpec<Dataset & {datasetId: string}>;
+const dropdownOpenStyle: CSSProperties = {
+  position: "absolute",
+  inset: "0px 0px auto auto",
+  margin: "0px",
+  transform: "translate(0px, 40px)",
+}
 
 export default function Datasets() {
+  const [actionIsOpen, setActionIsOpen] = useState(false); 
   const {datasets} = useData();
-  const datasetItems = datasets.all().map<DatasetTable>(dataset => ({
+  const datasetItems = datasets.all().map<TypedSearchableItem<Dataset & {datasetId: string}>>(dataset => ({
     id: dataset.id,
-    datasetId: <Link href={`/sdad/datasets/entry/?entryId=${dataset.id}`}>{dataset.id}</Link>,
-    date: toLocalDate(new Date(dataset.date)),
-    files: String(dataset.files),
-    size: String(dataset.size),
+    properties: {
+      datasetId: {
+        term: dataset.id,
+        component: <Link href={`/sdad/datasets/entry/?entryId=${dataset.id}`}>{dataset.id}</Link>,
+      },
+      date: {
+        term: toLocalDate(new Date(dataset.date)),
+        component: toLocalDate(new Date(dataset.date)),
+      },
+      files: {
+        term: String(dataset.files),
+        component: String(dataset.files),
+      },
+      size: {
+        term: String(dataset.size),
+        component: String(dataset.size),
+      }
+    }
   }));
 
   const datasetColumns: ColumnSpec<Dataset & {datasetId: string}> = {
@@ -26,10 +47,26 @@ export default function Datasets() {
     files: "Number of files",
     size: "Dataset size",
   };
-  const datasetPagination = usePagination(datasetItems, 30)
+  const {filter, setFilter, filteredItems} = useFilter(datasetItems)
+  const datasetPagination = usePagination(filteredItems, 30)
   return (
     <>
       <h2>Datasets</h2>
+      <div className="input-group mb-3">
+        <span className="input-group-text">Filter</span>
+        <input
+          type="text"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+          className="form-control"
+          placeholder={Object.values(datasetColumns).filter(c => !!c).join(", ")}
+          aria-label="Filter for actor table"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text flex-grow-1" >Viewing {filteredItems.length} of {datasetItems.length}</span>
+      </div>
       <Pagination pages={datasetPagination.pages} currentPage={datasetPagination.currentPage}/>
       <Table items={datasetPagination.items} columns={datasetColumns} />
       <Pagination pages={datasetPagination.pages} currentPage={datasetPagination.currentPage}/>
